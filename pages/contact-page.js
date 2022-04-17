a663d9995be61a4dd583432a87cc621d1d20f85c
u-masato <masato.u20@gmail.com>
2022-04-18 00:32:48 +0900

    changed to function

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 import Image from "next/image"
 
-const Contact = () => {
+export default function Contact() {
   return (
     <Layout title="Contact">
       <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
@@ -83,5 +83,3 @@ const Contact = () => {
     </Layout>
   );
 };
-
-export default Contact;
